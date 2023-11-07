0a6a5f2e98d1d08a32cf614628f4bbd4d3c0c169
JeanPoolDev <jeanpoolrojasavila456@gmail.com>
2023-11-07 10:33:29 -0500

    use useEffect

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+
+  const [enabled, setEnabled] = useState(false) 
+  const [position, setPosition] = useState({ x:0 , y:0 })
+
+  //* pointer move
+  useEffect(()=> {
+    console.log('effect', {enabled})
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      // console.log('HandleMove',{clientX, clientY})
+      setPosition({x: clientX, y: clientY})
+    }
+    if(enabled){
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      console.log('clean')
+      window.removeEventListener('pointermove', handleMove)
+      setPosition({ x:0 , y:0 })
+    }
+  }, [enabled])
+
+  //* change body classname
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+    
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  return (
+    <>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      border: '1px solid #fff',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -25,
+      top: -25,
+      width: 50,
+      height: 50,
+      transform: `translate(${position.x}px, ${position.y}px)`
+    }}
+    />
+    <button onClick={() => setEnabled(!enabled)}>
+      {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+    </button>
+
+    </>
+  )
+}
+
+function App() {
+
+  return (
+    <main>
+      <FollowMouse/>
+    </main>  
+  )
+}
+
+export default App
